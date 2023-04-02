66175a07d333885c386daf7e1158c0954a2047c6
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-04-02 18:42:41 +0200

    Ajout des emplacements photos galerie

--- FrontEnd/accueil.js
+++ FrontEnd/accueil.js
@@ -1,1 +1,23 @@
-const figure = photo[i];
+const reponse = await fetch("http://localhost:5678/api/works/");
+const photos = await reponse.json();
+
+console.log(photos);
+
+for(let i = 0; i < photos.length; i++) {
+    const figure = photos[i];
+    const sectionPortfolio = document.querySelector("#portfolio");
+    const travailElement = document.querySelector(".gallery");
+    const photoElement = document.createElement("figure");
+
+    const imageElement = document.createElement("img");
+    imageElement.src = figure.image;
+
+    const nomElement = document.createElement("figcaption");
+    nomElement.innerText = figure.nom;
+
+
+    sectionPortfolio.appendChild(photoElement)
+    photoElement.appendChild(imageElement);
+    photoElement.appendChild(nomElement);
+
+};
