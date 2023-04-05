e28538c898abda3b8996e9643e2fa462e43be41f
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-04-05 14:16:12 +0200

    maj boutons

--- FrontEnd/accueil.js
+++ FrontEnd/accueil.js
@@ -1,6 +1,15 @@
 const reponse = await fetch("http://localhost:5678/api/works/");
 const productsArray = await reponse.json();
 
+function SaveFilter(chat){
+    localStorage.setItem("filtre", chat);
+}
+
+
+
+
+
+
 console.log(productsArray);
 
 function CreateCard (chat) {
@@ -20,24 +29,39 @@ function CreateCard (chat) {
 
 };
 
-for(let product of productsArray) {
+if (localStorage.getItem("filtre")){
+    let readFiltre = localStorage.getItem("filtre");
+    console.log(readFiltre);
+    for( let i = 0; i < productsArray.length; i++){
+        if(productsArray[i].category.name === readFiltre){
+            CreateCard(productsArray[i]);
+        };
+    };
+} else { for(let product of productsArray) {
     CreateCard(product);
-}
+}};
 
-const reponseFilters = await fetch("http://localhost:5678/api/categories/");
-const filters = await reponseFilters.json();
+
+const filters = await fetch("http://localhost:5678/api/categories/")
+.then(filters => filters.json());
 
 console.log(filters);
+let filtre = "";
 
-for (let filter of filters) {
-    console.log(filter.name);
-};
+const boutonTous = document.getElementById("btn-tous");
+boutonTous.addEventListener("click", function() {
+    localStorage.removeItem("filtre");
+    location.reload();
+});
 
 const boutonObjets = document.getElementById("btn-objets");
 boutonObjets.addEventListener("click", function(){
-    for(let filter of filters){
-        if(filter.name == "Objets"){
-            console.log (filter.name);
+    const type = filters.map(filter => filter.name);
+        for( let i = filters.length -1; i >= 0; i--){
+            if(filters[i].name == "Objets"){
+                filtre = type.splice(i,1);
+                SaveFilter(filtre);
+                location.reload();
+            };
         };
-    };
 });
