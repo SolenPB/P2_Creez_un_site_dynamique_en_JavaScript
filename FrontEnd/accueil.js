e28083909bfab882f17b5a0259a456f7b8374e97
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-04-07 15:11:19 +0200

    Lien connexion

--- FrontEnd/accueil.js
+++ FrontEnd/accueil.js
@@ -103,5 +103,16 @@ boutonRestaurants.addEventListener("click", function () {
         };
 });
 
+//Fonctionnalité authentification 
+
+const emailValues = {
+    email: "sophie.bluel@test.tld",
+    password:"S0phie",
+};
+
+const boutonConnecter = document.getElementById("btn-connecter");
+boutonConnecter.addEventListener("click", function() {
+    
+});
 
   
---
+++ FrontEnd/connexion.js
@@ -0,0 +1,11 @@
+//Fonctionnalité authentification 
+
+const emailValues = {
+    email: "sophie.bluel@test.tld",
+    password:"S0phie",
+};
+
+const boutonConnecter = document.getElementById("btn-connecter");
+boutonConnecter.addEventListener("click", function() {
+    
+});
