d43d4a8a18403de90b2418489e80bbd5e426051a
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-04-14 10:13:21 +0200

    Ajout fermeture modale

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,11 +1,20 @@
+const modal = document.getElementById('modal1');
+
 const linkModal = document.getElementById('js-modal');
 linkModal.addEventListener("click", function(event) {
     console.log("Ça marche !");
     event.preventDefault();
-    const modal = document.getElementById('modal1');
         modal.style.display = null;
         modal.removeAttribute('aria-hidden');
         modal.setAttribute('aria-modal', 'true');
 });
 
+const closeModal = document.getElementById("closemodal");
+closeModal.addEventListener("click", function(event){
+    console.log("Va faire tout noir !");
+    event.preventDefault();
+        modal.style.display = "none";
+        modal.setAttribute('aria-hidden', 'true');
+        modal.removeAttribute('aria-modal');
+});
 
