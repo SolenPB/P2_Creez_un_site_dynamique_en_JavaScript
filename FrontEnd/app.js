a8365002658fd265757922b3543fcb828bec08f8
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-05-04 16:51:39 +0200

    Finition code

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -91,13 +91,14 @@ const closeModal = document.getElementById("closemodal");
                         iconDelete.style.position = "relative";
                         iconDelete.style.width = "15px";
                         iconDelete.style.height = "15px";
-                        iconDelete.backgroundColor = "black";
+                        iconDelete.style.backgroundColor = "black";
                         iconDelete.style.color = "white";
-                        iconDelete.style.paddingTop = "2px";
+                        iconDelete.style.paddingLeft = "3px";
+                        iconDelete.style.paddingTop = "3px";
                         iconDelete.style.border = "none";
                         iconDelete.style.width = "17px";
                         iconDelete.style.height = "17px";
-                        iconDelete.style.top = "11px";
+                        iconDelete.style.top = "9px";
                         iconDelete.style.left = "62px";
 
 
@@ -129,55 +130,7 @@ const closeModal = document.getElementById("closemodal");
                 pictureModal.appendChild(movePicture);
         };
 
-//Style du titre de la modale
-const titleModal = document.getElementById("titlemodal");
 
-                titleModal.style.height = "30px";
-                titleModal.style.fontFamily = "work sans";
-                titleModal.style.fontWeight = "400";
-                titleModal.style.color = "black";
-
-//Style des boutons supérieurs de la modale
-const buttonsModal = document.getElementById("btns-modal");
-                buttonsModal.style.width = "600px";
-                buttonsModal.style.display = "flex";
-                buttonsModal.style.justifyContent = "space-between";
-                buttonsModal.style.alignItems = "center";
-                buttonsModal.style.marginTop = "15px";
-
-        
-const buttonCloseModal = document.getElementById("closemodal");
-                buttonCloseModal.style.display = "flex";
-                buttonCloseModal.style.height = "24px";
-                buttonCloseModal.style.fontSize = "24px";
-                buttonCloseModal.style.backgroundColor = "white";
-                buttonCloseModal.style.border = "none";
-                buttonCloseModal.style.paddingBottom = "5px";
-
-const styleModalGallery = document.getElementById("gallery-modal");
-                styleModalGallery.style.marginTop = "40px";
-                styleModalGallery.style.paddingLeft = "65px";
-                styleModalGallery.style.width = "530px";
-                styleModalGallery.style.height = "450px" 
-                styleModalGallery.style.display = "flex";
-                styleModalGallery.style.flexDirection = "row";
-                styleModalGallery.style.flexWrap = "wrap";
-
-//Mise en forme de la galerie modale, du bouton ouvrant la seconde modale et du futur bouton de suppression
-
-const changeModal = document.getElementById("change-modal");
-                changeModal.style.display = "flex";
-                changeModal.style.flexDirection = "column";
-                changeModal.style.justifyContent = "center";
-                changeModal.style.width = "420px";
-                changeModal.style.borderTop = "solid", "black";
-                changeModal.style.marginBottom = "30px";
-
-const deleteGallery = document.getElementById("delete-gallery");
-                deleteGallery.style.display = "flex";
-                deleteGallery.style.justifyContent = "center";
-                deleteGallery.style.color = "black";
-                deleteGallery.style.textDecoration = "none";
 
 const buttonPreModal = document.getElementById("premodal");
                 buttonPreModal.style.border = "none";
@@ -230,26 +183,6 @@ const changePhoto = document.getElementById("change-photo");
 
 
 
-//Intégration de la modale d'ajout de photo
-
-const preModalTitle = document.getElementById("premodaltitle");
-                        preModalTitle.style.fontFamily = "work sans";
-                        preModalTitle.style.fontWeight = "400";
-                        preModalTitle.style.color = "black";
-                        preModalTitle.style.marginTop = "10px";
-
-//Formulaire d'ajout de nouvelles photos 
-
-const formPhoto = document.getElementById("formphoto");
-    const addPhoto = document.getElementById("newphoto");
-                        addPhoto.style.width = "420px";
-                        addPhoto.style.height = "180px";
-                        addPhoto.style.justifyContent = "center";
-                        addPhoto.style.alignItems = "center";
-                        addPhoto.style.flexDirection = "column";
-                        addPhoto.style.backgroundColor = "#E8F1F7";
-                        addPhoto.style.borderRadius = "5px";
-                        addPhoto.style.marginTop = "36px";
 
 //Prévisualisation de la photo et intégration de sa zone d'apparition
 
@@ -271,73 +204,7 @@ const formPhoto = document.getElementById("formphoto");
                         photo.style.border = "none";
                         photo.style.display = "none";
 
-    const iconPhoto = document.getElementById("icon");
-                        iconPhoto.style.fontSize = "58px";
-                        iconPhoto.style.justifyContent = "center";
-                        iconPhoto.style.color = "#B9C5CC";
-                        iconPhoto.style.marginTop = "10px";
-
-//Bouton d'ajout de photo
-
-    const actionButton = document.getElementById("buttonAdd");
-                        actionButton.style.display = "flex";
-                        actionButton.style.flexDirection = "column";
-                        actionButton.style.alignItems = "center";
-                        actionButton.style.borderRadius = "60px";
-                        actionButton.style.backgroundColor = "#A7A7A7";
-                        actionButton.style.height = "36px";
-                        actionButton.style.marginTop = "15px";
-
-
-    const buttonAddPhoto = document.getElementById("buttonaddphoto");
-                        buttonAddPhoto.textContent = "+ Ajouter photo";
-                        buttonAddPhoto.style.position = "relative";
-                        buttonAddPhoto.style.top = "12px";
-                        buttonAddPhoto.style.color = "#306685";
-    const inputAddPhoto = document.getElementById("input-addphoto");
-            inputAddPhoto.setAttribute("name", "max_file_size");
-            inputAddPhoto.setAttribute("value", "4096");
-                        inputAddPhoto.style.height = "36px";
-                        inputAddPhoto.style.opacity = "0";
-
-    const formatImage = document.getElementById("formatimage");
-                        formatImage.style.marginTop = "5px";
-
-
-
-//Entrée des informations pour la nouvelle image
-
-const infoPhoto = document.getElementById("infophoto");
-                        infoPhoto.style.flexDirection = "column";
-                        infoPhoto.style.height = "250px";
-                        infoPhoto.style.justifyContent = "center";
-//Entrée du titre de la nouvelle image
-
-const infoTitle = document.getElementById("infotitle");
-                        infoTitle.style.display = "flex";
-                        infoTitle.style.flexDirection = "column";
-const titleInput = document.getElementById("titrephoto");
-                        titleInput.style.marginBottom = "10px";
-const titlePhoto = document.getElementById("titlephoto");
-                        titlePhoto.style.height = "50px";
-                        titlePhoto.style.marginBottom = "20px";
-                        titlePhoto.style.border = "none";
-                        titlePhoto.style.boxShadow = "0 4px 14px rgba(0,0,0,0.09)";
-
-//Entrée de la catégorie de la nouvelle image
-
-const infoCat = document.getElementById("infocat");
-                        infoCat.style.display = "flex";
-                        infoCat.style.flexDirection = "column";
-const catTitle = document.getElementById("categorie");
-                        catTitle.style.marginBottom = "10px";
-
-const catPhoto = document.getElementById("catphoto");
-                        catPhoto.style.height = "50px";
-                        catPhoto.style.border = "none";
-                        catPhoto.style.boxShadow = "0 4px 14px rgba(0,0,0,0.09)";
-                        catPhoto.style.backgroundColor = "white";
-
+    
 const btnValidation = document.createElement("input");
         btnValidation.setAttribute("id", "btn-validation");
         btnValidation.setAttribute("type", "submit");   
@@ -402,6 +269,157 @@ const validation = document.getElementById("validation");
                 validation.style.borderTop = "solid", "black", "1px";
 
 
+
+
+
+
+
+
+
+
+
+//Intégration de la modale d'ajout de photo
+
+const preModalTitle = document.getElementById("premodaltitle");
+                        preModalTitle.style.fontFamily = "work sans";
+                        preModalTitle.style.fontWeight = "400";
+                        preModalTitle.style.color = "black";
+                        preModalTitle.style.marginTop = "10px";
+
+
+//Style du titre de la modale
+const titleModal = document.getElementById("titlemodal");
+
+                titleModal.style.height = "30px";
+                titleModal.style.fontFamily = "work sans";
+                titleModal.style.fontWeight = "400";
+                titleModal.style.color = "black";
+
+//Style des boutons supérieurs de la modale
+const buttonsModal = document.getElementById("btns-modal");
+                buttonsModal.style.width = "600px";
+                buttonsModal.style.display = "flex";
+                buttonsModal.style.justifyContent = "space-between";
+                buttonsModal.style.alignItems = "center";
+                buttonsModal.style.marginTop = "15px";
+
+        
+const buttonCloseModal = document.getElementById("closemodal");
+                buttonCloseModal.style.display = "flex";
+                buttonCloseModal.style.height = "24px";
+                buttonCloseModal.style.fontSize = "24px";
+                buttonCloseModal.style.backgroundColor = "white";
+                buttonCloseModal.style.border = "none";
+                buttonCloseModal.style.paddingBottom = "5px";
+
+const styleModalGallery = document.getElementById("gallery-modal");
+                styleModalGallery.style.marginTop = "40px";
+                styleModalGallery.style.paddingLeft = "65px";
+                styleModalGallery.style.width = "530px";
+                styleModalGallery.style.height = "450px" 
+                styleModalGallery.style.display = "flex";
+                styleModalGallery.style.flexDirection = "row";
+                styleModalGallery.style.flexWrap = "wrap";
+
+//Mise en forme de la galerie modale, du bouton ouvrant la seconde modale et du futur bouton de suppression
+
+const changeModal = document.getElementById("change-modal");
+                changeModal.style.display = "flex";
+                changeModal.style.flexDirection = "column";
+                changeModal.style.justifyContent = "center";
+                changeModal.style.width = "420px";
+                changeModal.style.borderTop = "solid", "black";
+                changeModal.style.marginBottom = "30px";
+
+const deleteGallery = document.getElementById("delete-gallery");
+                deleteGallery.style.display = "flex";
+                deleteGallery.style.justifyContent = "center";
+                deleteGallery.style.color = "black";
+                deleteGallery.style.textDecoration = "none";
+
+
+
+
+
+                
+//Formulaire d'ajout de nouvelles photos 
+
+const formPhoto = document.getElementById("formphoto");
+    const addPhoto = document.getElementById("newphoto");
+                        addPhoto.style.width = "420px";
+                        addPhoto.style.height = "180px";
+                        addPhoto.style.justifyContent = "center";
+                        addPhoto.style.alignItems = "center";
+                        addPhoto.style.flexDirection = "column";
+                        addPhoto.style.backgroundColor = "#E8F1F7";
+                        addPhoto.style.borderRadius = "5px";
+                        addPhoto.style.marginTop = "36px";
+
+
+const iconPhoto = document.getElementById("icon");
+                iconPhoto.style.fontSize = "58px";
+                iconPhoto.style.justifyContent = "center";
+                iconPhoto.style.color = "#B9C5CC";
+                iconPhoto.style.marginTop = "10px";
+
+//Bouton d'ajout de photo
+const actionButton = document.getElementById("buttonAdd");
+                actionButton.style.display = "flex";
+                actionButton.style.flexDirection = "column";
+                actionButton.style.alignItems = "center";
+                actionButton.style.borderRadius = "60px";
+                actionButton.style.backgroundColor = "#A7A7A7";
+                actionButton.style.height = "36px";
+                actionButton.style.marginTop = "15px";
+
+
+const buttonAddPhoto = document.getElementById("buttonaddphoto");
+                buttonAddPhoto.textContent = "+ Ajouter photo";
+                buttonAddPhoto.style.position = "relative";
+                buttonAddPhoto.style.top = "12px";
+                buttonAddPhoto.style.color = "#306685";
+const inputAddPhoto = document.getElementById("input-addphoto");
+    inputAddPhoto.setAttribute("name", "max_file_size");
+    inputAddPhoto.setAttribute("value", "4096");
+                inputAddPhoto.style.height = "36px";
+                inputAddPhoto.style.opacity = "0";
+
+const formatImage = document.getElementById("formatimage");
+                formatImage.style.marginTop = "5px";
+
+//Entrée des informations pour la nouvelle image
+
+const infoPhoto = document.getElementById("infophoto");
+                infoPhoto.style.flexDirection = "column";
+                infoPhoto.style.height = "250px";
+                infoPhoto.style.justifyContent = "center";
+//Entrée du titre de la nouvelle image
+
+const infoTitle = document.getElementById("infotitle");
+                infoTitle.style.display = "flex";
+                infoTitle.style.flexDirection = "column";
+const titleInput = document.getElementById("titrephoto");
+                titleInput.style.marginBottom = "10px";
+const titlePhoto = document.getElementById("titlephoto");
+                titlePhoto.style.height = "50px";
+                titlePhoto.style.marginBottom = "20px";
+                titlePhoto.style.border = "none";
+                titlePhoto.style.boxShadow = "0 4px 14px rgba(0,0,0,0.09)";
+
+//Entrée de la catégorie de la nouvelle image
+
+const infoCat = document.getElementById("infocat");
+                infoCat.style.display = "flex";
+                infoCat.style.flexDirection = "column";
+const catTitle = document.getElementById("categorie");
+                catTitle.style.marginBottom = "10px";
+
+const catPhoto = document.getElementById("catphoto");
+                catPhoto.style.height = "50px";
+                catPhoto.style.border = "none";
+                catPhoto.style.boxShadow = "0 4px 14px rgba(0,0,0,0.09)";
+                catPhoto.style.backgroundColor = "white";
+
 //Rattachement des éléments de la modale avec les parents 
 
 formPhoto.appendChild(addPhoto);
@@ -420,9 +438,4 @@ infoTitle.appendChild(titlePhoto);
 infoCat.appendChild(catTitle);
 infoCat.appendChild(catPhoto);
 validation.appendChild(btnValidation);
-
-
-
-
-
 
