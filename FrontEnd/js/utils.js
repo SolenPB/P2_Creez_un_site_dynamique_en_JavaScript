4fd74ec7d312c5b4a272f41f193c8b0455d56a33
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-05-12 11:30:58 +0200

    maj dossier

--- FrontEnd/js/utils.js
+++ FrontEnd/js/utils.js
@@ -41,25 +41,3 @@ export function Filtered(filter, data) {
 
 };
 
-export function Added (works, data) { 
-        let addedWork = "";
-        if(works !== ""){
-            addedWork = data.find(obj);
-        } else {
-            addedWork = data;
-        }
-            let gallery = document.querySelector(".gallery");
-            let galleryModal = document.getElementById("gallery-modal");
-            let galleryChildren = document.getElementsByClassName('work1');
-            let modalChildren = document.getElementById("photomodal");
-         
-           for (let i = -galleryChildren.length; i < galleryChildren.length; i++) {
-           
-            gallery.removeChild(gallery.firstChild);
-            
-           };
-           for(let i = 0; i < addedWork.length; i++){
-            CreateCard(addedWork[i]);
-           }
-
-};
